a5a5a08c5127b3be667f01360ac08e60d67edfb6
Marija Stojanovic <marijastojanovic5@yahoo.com>
2019-10-03 16:12:40 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -84,7 +84,10 @@ def adding_matz
         }
      }
      
-     programmer_hash[0][:yukihiro_matsumoto] ={} 
+     programmer_hash[0][:yukihiro_matsumoto] ={
+  :known_for => "Ruby",
+    :languages => ["LISP", "C"]
+ } 
 
 
     
